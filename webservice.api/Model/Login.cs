81c0a2105b78f58706278d5008be69732244618f
Indu Kumar <indu.kumar@xceedance.com>
2017-08-16 18:49:42 +0530

    new file

---
+++ webservice.api/Model/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webservice.api.Model
+{
+    public class Login
+    {
+        public string Mail { get; set; }
+
+    }
+}
